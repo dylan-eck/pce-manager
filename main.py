98f821d839ace106d58b755bf2ee5d2b38ccb62f
Dylan Eck <dylaneck@mymail.mines.edu>
2021-02-11 15:53:06 -0700

    fixed next_time calculation in main while loop

--- main.py
+++ main.py
@@ -12,29 +12,27 @@ if __name__ == '__main__':
        
         current_time = time.localtime()
         next_time = timesheet.get_next_time(current_time)
-        print("next time:", next_time)
 
         time_slot = timesheet.times.index(next_time) - 1
 
         current_state = timesheet.states[time_slot]
         port.write(chr(current_state).encode())
         print("initial state:", current_state)
+        print("next time:", next_time, '\n')
 
         while True:
-            #print("current_time:", current_time.tm_hour*100 + current_time.tm_min)
-            #print("next_time:", next_time)
             current_time = time.localtime()
             time_num = current_time.tm_hour*100 + current_time.tm_min
             if time_num >= next_time:
-                print("event reached")
+                print("event reached at", time_num)
                 if (time_slot < (len(timesheet.times) - 1)):
                     time_slot += 1
                 else:
                     time_slot = 0
                 current_state = timesheet.states[time_slot]
+                next_time = timesheet.times[time_slot + 1]
                 print("new state:", current_state)
-                next_time = timesheet.times[time_slot]
-                print(next_time)
+                print("next time:", next_time, '\n')
                 port.write(chr(current_state).encode())
             time.sleep(0.5)
     else:
